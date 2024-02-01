6df73e57a69eadb956c0510e67ec6eccfc95de87
ojasvita-novice <ojasvitawagisha@gmail.com>
2024-02-01 15:56:22 +0530

    first_commit

---
+++ setup/src/Review.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+
+const [index,setIndex]=useState(0);
+const {name,job,image,text}=people[index];
+
+const checkNumber=(number)=>
+{
+  if(number>people.length-1)
+  {
+    return 0;
+  }
+  if(number<0)
+  {
+    return people.length-1;
+  }
+  return number;
+}
+
+
+const nextBtn=(index)=>
+{
+  setIndex((index)=>
+  {
+     let newIndex = index + 1;
+     return checkNumber(newIndex);
+  });
+}
+
+
+const prevBtn=(index)=>
+{
+ setIndex((index) => {
+   let newIndex = index - 1;
+   return checkNumber(newIndex);
+ });
+}
+
+const random=()=>
+{
+  let randomIndex=Math.floor(Math.random()*people.length-1)
+  return setIndex(checkNumber(randomIndex));
+}
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={() => prevBtn(index)}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={() => nextBtn(index)}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={() => random()}>
+        Random Profile
+      </button>
+    </article>
+  );
+};
+
+export default Review;
